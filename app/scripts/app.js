4e5c81af357e53f34abe863f6fcfd3f7d91cc687
to0 <yuanancai@gmail.com>
2014-12-07 05:07:48 -0500

    for NGN project

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,59 +8,52 @@
  *
  * Main module of the application.
  */
-// angular
-//   .module('UniversityLocatorApp', [
-//     'ngAnimate',
-//     // 'ngCookies',
-//     // 'ngResource',
-//     'ngTouch',
-//     'ngAnimate',
-//     'ui.bootstrap'
-//   ]);
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var universities = ['Columbia University', 'New York University', 'Stony Brook University', 'City University of New York'];
+var nameToLink = {
+  'Columbia University': 'columbia',
+  'New York University': 'nyu',
+  'Stony Brook University': 'stony',
+  'City University of New York': 'cuny'
+};
+
+var appHost = 'http://app.roomhunter.us';
+
 $( document ).ready(function($) {
-        var substringMatcher = function(strs) {
-                return function findMatches(q, cb) {
-                        var matches, substrRegex;
-
-                        // an array that will be populated with substring matches
-                        matches = [];
-
-                        // regex used to determine if a string contains the substring `q`
-                        substrRegex = new RegExp(q, 'i');
-
-                        // iterate through the pool of strings and for any string that
-                        // contains the substring `q`, add it to the `matches` array
-                        $.each(strs, function(i, str) {
-                                if (substrRegex.test(str)) {
-                                        // the typeahead jQuery plugin expects suggestions to a
-                                        // JavaScript object, refer to typeahead docs for more info
-                                        matches.push({ value: str });
-                                }
-                        });
-
-                        cb(matches);
-                };
-        };
-
-        var universities = ['Columbia University', 'New York University', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-        ];
-
-        $('.university-typeahead').typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 1
-        },
-        {
-                name: 'states',
-                displayKey: 'value',
-                source: substringMatcher(universities)
-        });
+  $('.university-typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'states',
+    displayKey: 'value',
+    source: substringMatcher(universities)
+  });
+  $('#search-btn').click(function(e){
+    window.location = appHost + '/#/li/' + nameToLink[$('#search-str').val()];
+  });
 });

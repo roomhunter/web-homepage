2420f53d98c81cf5f8122b587bf2c90776f4bd13
terrence <lixiy1234@gmail.com>
2015-01-24 20:04:55 -0500

    finish login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,6 +42,8 @@ var nameToLink = {
 
 var appHost = 'http://app.roomhunter.us';
 
+$('.newLink').hide();
+
 $( document ).ready(function($) {
   $('.university-typeahead').typeahead({
     hint: true,
@@ -56,4 +58,134 @@ $( document ).ready(function($) {
   $('#search-btn').click(function(e){
     window.location = appHost + '/#/li/' + nameToLink[$('#search-str').val()];
   });
+
+
+  $('#register-form').submit(function(e){
+    var email = $('#emailaddress').val();
+    var pwd = $('#password').val();
+    if(email==""&&pwd=="") {
+      $('.form-group').addClass("has-error");
+      e.preventDefault();
+      $('#msg').show("slow").html("The above fields cannot be blank!");
+    return;
+    }
+    else if(email=="") {
+      $('#emailaddress').addClass("has-error");
+      e.preventDefault();
+      $('#msg').show("slow").html("Email address cannot be blank!");
+      return;
+    }
+    else if(pwd=="") {
+      $('#password').addClass("has-error");
+      e.preventDefault();
+      $('#msg').show("slow").html("Password cannot be blank!");
+      return;
+    }
+    var end = email.split(".");
+    if(end.length==1) {$('#emailaddress').addClass("has-error");$('#msg').show("slow").html("Invalid email address!");e.preventDefault();return;}
+    if(end[1]!="edu") {$('#emailaddress').addClass("has-error");$('#msg').show("slow").html("You must register with an .edu email address!");e.preventDefault();return;};
+    <!-- use ajax to submit -->
+    var postData = $('#register-form').serialize();
+    $('.form-group').removeClass("has-error");
+    /*$.ajax({
+      cache:true,
+      type:post,
+      url:$('register-form').attr("action"),
+      data:postData,
+      dataType:json,
+      success:function(data){
+        alert("aaa");
+
+
+      },
+      error:function(){
+        alert("aa");
+
+
+      }
+
+    });*/
+    e.preventDefault();
+    $('#RegisterModal').modal('hide');
+    $('#InformationModal1').modal('show');
+  })
+
+
+  $('#login-form').submit(function(e){
+    var email = $('#emailaddress1').val();
+    var pwd = $('#password1').val();
+    if(email==""&&pwd=="") {
+      $('.form-group').addClass("has-error");
+      e.preventDefault();
+      $('#msg2').show("slow").html("The above fields cannot be blank!");
+      return;
+    }
+    else if(email=="") {
+      $('#emailaddress').addClass("has-error");
+      e.preventDefault();
+      $('#msg2').show("slow").html("Email address cannot be blank!");
+      return;
+    }
+    else if(pwd=="") {
+      $('#password').addClass("has-error");
+      e.preventDefault();
+      $('#msg2').show("slow").html("Password cannot be blank!");
+      return;
+    }
+    $.ajax({
+      cache:true,
+      type:post,
+      url:$('login-form').attr("action"),
+      data:postData,
+      dataType:json,
+      success:function(data){
+        $('.oldLink').hide();
+        location.reload();
+        $('.newLink').find('a').attr("href","/profile?username="+username).html(username);
+        $('.newLink').show();
+      },
+      error:function(){
+        alert("aa");
+      }
+
+    })
+  })
+
+  $('#personal-form-1').submit(function(e){
+    var first = $('#firstname').val();
+    var last = $('#lastname').val();
+    var uni = $('university').val()
+    if(first==""&&last=="") {
+      $('.form-group').addClass("has-error");
+      e.preventDefault();
+      $('#msg3').show("slow").html("The above fields cannot be blank!");
+      return;
+    }
+    else if(first=="") {
+      $('#firstname').addClass("has-error");
+      e.preventDefault();
+      $('#msg3').show("slow").html("First name cannot be blank!");
+      return;
+    }
+    else if(last=="") {
+      $('#lastname').addClass("has-error");
+      e.preventDefault();
+      $('#msg3').show("slow").html("Last name cannot be blank!");
+      return;
+    }
+    $.ajax({
+      cache:true,
+      type:post,
+      url:$('information-form-1').attr("action"),
+      data:postData,
+      dataType:json,
+      success:function(data){
+        location.reload();
+      },
+      error:function(){
+        alert("aa");
+      }
+
+    })
+  })
 });

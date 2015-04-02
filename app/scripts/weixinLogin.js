8033779cd4075e89f09b64d13f3cd473ed7cab2a
terrence <lixiy1234@gmail.com>
2015-04-02 13:52:51 -0400

    stylized wechat login iframe

--- app/scripts/weixinLogin.js
+++ app/scripts/weixinLogin.js
@@ -6,15 +6,29 @@
 var weixinLogin = {
 
     initialization : function () {
-      var objj = new WxLogin({
-        id:"wechat-twoD-code",
+      var wechat_login = new WxLogin({
+        id:"wechat-login-2D-code",
         appid: "wxc685011a4fb095bd",
         scope: "snsapi_login",
         redirect_uri: encodeURIComponent("http://roomhunter.us"),
         state: "",
         style: "black",
-        //href: "/styles/wechat-related.css"
+        href: "http://localhost:9000/styles/wechat-related.css"
       });
+
+      var wechat_register = new WxLogin({
+        id:"wechat-register-2D-code",
+        appid: "wxc685011a4fb095bd",
+        scope: "snsapi_login",
+        redirect_uri: encodeURIComponent("http://roomhunter.us"),
+        state: "",
+        style: "black",
+        href: "http://localhost:9000/styles/wechat-related.css"
+      });
+
+      $("iframe").each(function () {
+        $(this).css("height", "300px");
+      })
 
     }
 }

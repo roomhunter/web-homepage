6c73087c3f7dca7d3364170df2f9ffaa4039b625
to0 <yuanancai@gmail.com>
2015-04-22 03:46:55 -0400

    video strategy

--- app/scripts/video-control.js
+++ app/scripts/video-control.js
@@ -1,14 +1,17 @@
 'use strict';
 var video1 = document.getElementsByTagName("video")[0];
 // video's default 'canplaythrough' doesn't work well
-video1.addEventListener('progress', function (){
-  if(video1.buffered.length < 1) {
-    return;
-  }
-  var percentage = video1.buffered.end(0) / video1.duration;
-  if (percentage > 0.3) {
-    video1.play();
-    video1.removeEventListener('progress');
-  }
-});
+video1.waiting = function () {
+  video1.stop();
+  video1.progress = function (){
+    if(video1.buffered.length < 1) {
+      return;
+    }
+    var percentage = video1.buffered.end(0) / video1.duration;
+    if (percentage > 0.3) {
+      video1.play();
+      video1.removeEventListener('progress');
+    }
+  };
+};
 

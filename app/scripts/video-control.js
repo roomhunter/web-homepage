5da2a1ad5882b4c6b327d08ba63e2f24434dbbf1
to0 <yuanancai@gmail.com>
2015-04-22 00:48:31 -0400

    temporarily ease the video loading

---
+++ app/scripts/video-control.js
@@ -0,0 +1,14 @@
+'use strict';
+var video1 = document.getElementsByTagName("video")[0];
+// video's default 'canplaythrough' doesn't work well
+video1.addEventListener('progress', function (){
+  if(video1.buffered.length < 1) {
+    return;
+  }
+  var percentage = video1.buffered.end(0) / video1.duration;
+  if (percentage > 0.3) {
+    video1.play();
+    video1.removeEventListener('progress');
+  }
+});
+
